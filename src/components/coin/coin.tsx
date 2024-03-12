3c29a34099149e3dc79b6a3b383006c324adb2b7
Melonges <melonges@icloud.com>
2024-03-12 22:10:02 +0300

    dynamyc coin size

--- src/components/coin/coin.tsx
+++ src/components/coin/coin.tsx
@@ -1,15 +1,39 @@
 import Spline from "@splinetool/react-spline";
 import s from "./coin.module.css";
-export const Coin = () => (
-  <div>
-    <Spline
-      style={{ width: "500px", height: "500px" }}
-      className={s["main-coin"]}
-      scene={
-        window.innerWidth > 500
-          ? "https://prod.spline.design/3gv92v6EJvS45AIL/scene.splinecode"
-          : "https://prod.spline.design/eVHNDmMg-5EMjId6/scene.splinecode"
-      }
-    />
-  </div>
-);
+import { useEffect, useState } from "react";
+
+function getWindowDimensions() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  return {
+    width,
+    height,
+  };
+}
+export const Coin = () => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions(),
+  );
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div>
+      <Spline
+        style={{ width: "500px", height: "500px" }}
+        className={s["main-coin"]}
+        scene={
+          windowDimensions.width > 500
+            ? "https://prod.spline.design/3gv92v6EJvS45AIL/scene.splinecode"
+            : "https://prod.spline.design/eVHNDmMg-5EMjId6/scene.splinecode"
+        }
+      />
+    </div>
+  );
+};
