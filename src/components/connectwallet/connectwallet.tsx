27cb92ba86d5a4922ddc929f4e5098becf8dff24
Melonges <melonges@icloud.com>
2024-03-14 17:13:51 +0300

    fix text align

--- src/components/connectwallet/connectwallet.tsx
+++ src/components/connectwallet/connectwallet.tsx
@@ -10,10 +10,10 @@ export const ConnectWallet = () => {
         TrustBet On-Chain is a gamechanger in the online iGaming industry
         designed to transform the conventional iGaming experience.
       </h2>
-      <p>
-        We are pioneering a future of fairness, transparency, and
-        <br /> equilibrium in the digital iGaming realm. Be part of this
-        <br /> exciting journey as we redefine the iGaming experience
+      <p style={{ textAlign: "center", maxWidth: "500px" }}>
+        We are pioneering a future of fairness, transparency, and equilibrium in
+        the digital iGaming realm. Be part of this exciting journey as we
+        redefine the iGaming experience
       </p>
       <ConnectWalletButton className="connect-wallet-btn" />
     </section>
