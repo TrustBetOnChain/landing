7ed7b4f5fe25501220b56a5848e70b4cb5f988c5
yanmarinich <yanmarinich@gmail.com>
2024-04-05 02:04:18 +0300

    remove unused vars

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,1 @@
 export const NETWORK = import.meta.env.VITE_NETWORK;
-export const ALCHEMY_ID = import.meta.env.VITE_ALCHEMY_ID;
-export const WALLET_CONNECT_PROJECT_ID = import.meta.env
-  .VITE_WALLETCONNECT_PROJECT_ID;
