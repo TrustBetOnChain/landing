b43e524e7be55868b8cf03a174b9b15533aae8bd
yanmarinich <yanmarinich@gmail.com>
2024-04-10 18:42:21 +0300

    add cluster logger

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import App from "./App.tsx";
 import "./index.css";
 import "@dotlottie/react-player/dist/index.css";
 import { WalletConnectProvider } from "./WalletConnectProvider";
+import { CLUSTER } from "./presale/config/vars.ts";
+import { ENVIRONMENT } from "./constants/index.ts";
+
+console.log("CLUSTER:", CLUSTER);
+console.log("ENVIRONEMNT:", ENVIRONMENT);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
