b1bd2877a38ee8a40277f1800aa7484a94ea9d99
yanmarinich <yanmarinich@gmail.com>
2024-04-26 15:54:10 +0300

    fix account z-inedx

--- src/components/accountmodal/accountmodal.tsx
+++ src/components/accountmodal/accountmodal.tsx
@@ -38,7 +38,7 @@ export const AccountModal: React.FC<Props> = ({ isOpen, onClose }) => {
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
+      <Dialog as="div" className="relative z-[1000]" onClose={onClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
