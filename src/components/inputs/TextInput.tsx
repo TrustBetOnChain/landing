c5f0931358800f55345be748551aba51f6cd3267
Aliaksandr Sychou <aliaksandr.sychou@interexy.com>
2024-04-30 11:09:25 +0200

    fixed cur buttons refs

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -24,7 +24,7 @@ export const TextInput = React.forwardRef<HTMLInputElement, Props>(
             {...props}
             id={label}
             autoComplete={label}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
           />
         </div>
         {error && <p className="text-xs text-red-600">{error.message}</p>}
