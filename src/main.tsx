87b2a822dda08a825a8a1cec33c1be5e327819f7
yanmarinich <yanmarinich@gmail.com>
2024-05-04 00:49:31 +0300

    remove wallet connect console

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,6 @@ import {
 console.log("CLUSTER:", CLUSTER);
 console.log("ORIGIN:", ORIGIN);
 console.log("ENVIRONEMNT:", ENVIRONMENT);
-console.log("WALLETCONNECT_ID:", WALLETCONNECT_PROJECT_ID);
 console.log("ENDPOINT:", ENDPOINT);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
