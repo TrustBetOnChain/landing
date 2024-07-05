48d2d0021d5fa84aa02aa9e4a88f45aef5c2f964
aniket-solulab <aniket@solulab.co>
2024-07-05 15:09:24 +0530

    quick fixes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,4 +4,6 @@ export const ENVIRONMENT = "production";
 export const WALLETCONNECT_PROJECT_ID = import.meta.env
   .VITE_WALLETCONNECT_PROJECT_ID;
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export const ORIGIN = `https://${ENVIRONMENT === "staging" ? "staging." : ""}trustbetonchain.com/`;
