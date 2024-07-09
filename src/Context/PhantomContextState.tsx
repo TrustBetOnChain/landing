a83be2e035a755bb443cf17c2d7d76d1d631683b
aniket-solulab <aniket@solulab.co>
2024-07-09 15:43:25 +0530

    redeploy the domain link

--- src/Context/PhantomContextState.tsx
+++ src/Context/PhantomContextState.tsx
@@ -90,8 +90,8 @@ const PhantomContextState: FC<{ children: ReactNode }> = ({ children }) => {
     if (!("phantom" in window)) {
       return window.open(
         // "https://phantom.app/ul/browse?url=htps://trustbetonchain.com&ref=app.phantom",
-        "https://phantom.app/ul/browse/landing-git-feature-fixtransaction-trust-bet.vercel.app/?ref=https://https://landing-git-feature-fixtransaction-trust-bet.vercel.app//",
-        // "https://phantom.app/ul/browse/landing-git-feature-phantomstaging-trust-bet.vercel.app?ref=https://landing-git-feature-phantomstaging-trust-bet.vercel.app/",
+        // "https://phantom.app/ul/browse/landing-git-feature-fixtransaction-trust-bet.vercel.app/?ref=https://https://landing-git-feature-fixtransaction-trust-bet.vercel.app//",
+        "https://phantom.app/ul/browse/landing-git-feature-phantomstaging-trust-bet.vercel.app?ref=https://landing-git-feature-phantomstaging-trust-bet.vercel.app/",
         "_blank",
       );
     }
