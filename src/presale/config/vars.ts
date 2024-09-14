512f63b876ef0b3394a8321ddda16cfd866b54e1
aniket-solulab <aniket@solulab.co>
2024-09-14 13:34:50 +0530

    alem id updaed

--- src/presale/config/vars.ts
+++ src/presale/config/vars.ts
@@ -10,7 +10,7 @@ export const CLUSTER = "mainnet-beta" as SupportedNetwork;
 export const PROGRAM_IDL = IDL;
 
 export const PRIVATE_MAINNET_CLUSTER_URL =
-  "https://solana-mainnet.g.alchemy.com/v2/DBnynQyLa1gy-Mp2Xu1TjwlkQTfrDZMQ";
+  "https://solana-mainnet.g.alchemy.com/v2/snfGDgTvsSYiyOJ2az-5mvNYuxbHvN5k";
 
 export const ENDPOINT =
   CLUSTER === "devnet" ? clusterApiUrl(CLUSTER) : PRIVATE_MAINNET_CLUSTER_URL;
